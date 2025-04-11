b255e8b5728695a06262b1a47dea590a94765112
Amir Akbari <akbariindustries@gmail.com>
2025-04-11 14:02:11 +0330

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,29 @@ public class Main {
     private static char currentPlayer = 'X';
 
     public static void main(String[] args) {
-         PrintBoard();
         boolean gameRunning = true;
+
+        while (gameRunning) {
+            printBoard();
+            playerMove();
+            if (isWinner()) {
+                printBoard();
+                System.out.println("Player " + currentPlayer + " won the game!");
+                gameRunning = false;
+            } else if (isTie()) {
+                printBoard();
+                System.out.println("The game ended in a draw.!");
+                gameRunning = false;
+            } else {
+                switchPlayer();
+            }
+        }
     }
 
-
-
-    public static void PrintBoard() {
+    private static void printBoard() {
+        System.out.println("  0   1   2 ");
         for (int i = 0; i < 3; i++) {
-            System.out.print("  ");
+            System.out.print(i + " ");
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j]);
                 if (j < 2) System.out.print(" | ");
@@ -26,25 +40,55 @@ public class Main {
             if (i < 2) System.out.println(" ---+---+---");
         }
     }
-    private static void SwitchPlayer() {
-        currentPlayer = (currentPlayer=='X')?'O':'X';
+
+    private static void playerMove() {
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
+
+        while (true) {
+            System.out.println("Player " + currentPlayer + " it's your turn  Enter the row and column coordinates (example: 0 1):");
+            row = scanner.nextInt();
+            col = scanner.nextInt();
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+                board[row][col] = currentPlayer;
+                break;
+            } else {
+                System.out.println("Invalid, Try again!");
+            }
+        }
     }
-    private static boolean isTie()
-    {
-        for(int i = 0; i < board.length; i++)
-        {
-            for(int j = 0; j < board[i].length; j++)
-            {
-                if(board[i][j] == ' ')return false;
+    private static boolean isWinner() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true;
+            }
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true;
+            }
+        }
+
+        if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) {
+            return true;
+        }
+        if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean isTie() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
             }
         }
         return true;
     }
-    private static boolean isWinner()
-    {
-       return true;
+
+    private static void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
     }
-
-
-
 }
